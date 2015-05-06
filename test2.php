9ef470754ead83df796975798abfd361c905cc3a
evoup <madhouse_basedata@126.com>
2015-05-06 10:45:50 +0800

    检查所有广州的

--- test2.php
+++ test2.php
@@ -17,18 +17,31 @@ $total_madhouse_guangzhou=sizeof($MadhouseGuangZhouIps);
 echo "Madhouse IP库中广州的IPC网段有：{$total_madhouse_guangzhou}个\n";
 /*}}}*/
 
+/*{{{载入IPB小组的地域数据*/
+$ipbIps=file(dirname(__FILE__)."/other/ipb_district.csv");
+foreach($ipbIps as $line){
+    list($provinceName,$cityName,$districtId)=explode(',',$line);
+    $districtId=str_replace(array("\n","\r","\r\n"),"",$districtId);
+    $Ipb_dist_dic[$districtId]['provinceName']=$provinceName;
+    $Ipb_dist_dic[$districtId]['cityName']=$cityName;
+}
+/*}}}*/
+
 $test_file=file(dirname(__FILE__)."/nuohua-VE-APP5.2-IP.csv");
 $test_file=array_slice($test_file, 2, -1); // 验证所有样例中广州IP是否在MADHOUSE的IP库中也为广州
 foreach($test_file as $line){
     list($adpositionId, $adpositionName, $testIp, $districtId, $districtName, $numImps)=explode(',', $line);
-    if (strstr($adpositionName,'广州')) {
-        $ipc=netRange($testIp,25);
-        if (in_array($ipc,$MadhouseGuangZhouIps)) {
-            $Y++;
-            echo "检测到样例中的广州IP{$testIp} C网段：{$ipc}, 是否在MADHOUSE的IP库中存在：Y\n";
-        } else {
-            $N++;
-            echo "检测到样例中的广州IP{$testIp} C网段：{$ipc}, 是否在MADHOUSE的IP库中存在：N\n";
+    if ( in_array($districtId,array_keys($Ipb_dist_dic)) ) {
+        if ( $Ipb_dist_dic[$districtId]['cityName']=='广州市' ) {
+            $ipc=netRange($testIp,25);
+            echo $line;
+            if (in_array($ipc,$MadhouseGuangZhouIps)) {
+                $Y++;
+                echo "检测到样例中的广州IP{$testIp} C网段：{$ipc}, 是否在MADHOUSE的IP库中存在：Y\n";
+            } else {
+                $N++;
+                echo "检测到样例中的广州IP{$testIp} C网段：{$ipc}, 是否在MADHOUSE的IP库中存在：N\n";
+            }
         }
     }
 }
